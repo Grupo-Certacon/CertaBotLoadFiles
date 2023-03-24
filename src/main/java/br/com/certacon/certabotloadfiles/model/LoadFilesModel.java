f436ab0a520a988c34a5529e6fc8231b4d2b4b35
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-24 16:13:03 -0300

    chore: adição de propriedades para model

--- src/main/java/br/com/certacon/certabotloadfiles/model/LoadFilesModel.java
+++ src/main/java/br/com/certacon/certabotloadfiles/model/LoadFilesModel.java
@@ -15,11 +15,17 @@ public class LoadFilesModel {
     @Column(name = "load_file_id")
     private Long id;
 
-    @Column(name = "pasta", nullable = false)
-    private String folder;
+    @Column(name = "pasta_servidor", nullable = false)
+    private String serverFolder;
 
-    @Column(name = "servidor", nullable = false)
-    private String server;
+    @Column(name = "pasta_cnpj", nullable = false)
+    private String cnpjFolder;
+
+    @Column(name = "pasta_ano", nullable = false)
+    private String yearFolder;
+
+    @Column(name = "caminho")
+    private String path;
 
     @Column(name = "criado_em", nullable = false)
     private Date createdAt;
