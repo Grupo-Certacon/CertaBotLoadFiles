ee854b233a8635c96af96cd3f5202cf8ad23d775
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-24 14:40:13 -0300

    feat: initial commit

---
+++ src/main/java/br/com/certacon/certabotloadfiles/CertaBotLoadFilesApplication.java
@@ -0,0 +1,13 @@
+package br.com.certacon.certabotloadfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertaBotLoadFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertaBotLoadFilesApplication.class, args);
+    }
+
+}
