7bc3b86bb897e70d4f1bc2348a15be35f3333dd5
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-24 15:21:19 -0300

    test: criação de beforeEach e AfterEach

--- src/test/java/br/com/certacon/certabotloadfiles/serviceTest/CheckFolderExistServiceTest.java
+++ src/test/java/br/com/certacon/certabotloadfiles/serviceTest/CheckFolderExistServiceTest.java
@@ -2,6 +2,8 @@ package br.com.certacon.certabotloadfiles.serviceTest;
 
 import br.com.certacon.certabotloadfiles.repository.LoadFilesRepository;
 import br.com.certacon.certabotloadfiles.service.CheckFolderExistService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -12,4 +14,13 @@ public class CheckFolderExistServiceTest {
     CheckFolderExistService checkFolderExistService;
     @MockBean
     LoadFilesRepository checkFolderRepository;
+    @BeforeEach
+    void setUp() {
+        checkFolderExistService = new CheckFolderExistService(checkFolderRepository);
+    }
+
+    @AfterEach
+    void tearDown() {
+        checkFolderExistService = null;
+    }
 }
