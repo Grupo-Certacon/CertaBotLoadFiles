885363b8e24b3a7e0f48f23a2120b79052b05dfa
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-27 11:24:53 -0300

    chore: alteração sobre tipagem do id e adição de propriedade

--- src/main/java/br/com/certacon/certabotloadfiles/model/LoadFilesModel.java
+++ src/main/java/br/com/certacon/certabotloadfiles/model/LoadFilesModel.java
@@ -1,19 +1,23 @@
 package br.com.certacon.certabotloadfiles.model;
 
+import br.com.certacon.certabotloadfiles.utils.StatusFile;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class LoadFilesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "load_file_id")
-    private Long id;
+    private UUID id;
 
     @Column(name = "pasta_servidor", nullable = false)
     private String serverFolder;
@@ -26,6 +30,10 @@ public class LoadFilesModel {
 
     @Column(name = "caminho")
     private String path;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private StatusFile status;
 
     @Column(name = "criado_em", nullable = false)
     private Date createdAt;
