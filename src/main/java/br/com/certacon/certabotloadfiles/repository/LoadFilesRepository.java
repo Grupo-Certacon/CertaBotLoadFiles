9e18b11eaeec10497a1a8a67a52895e44c56c1b5
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-27 11:25:07 -0300

    chore: correção sobre tipagem do id

--- src/main/java/br/com/certacon/certabotloadfiles/repository/LoadFilesRepository.java
+++ src/main/java/br/com/certacon/certabotloadfiles/repository/LoadFilesRepository.java
@@ -4,6 +4,8 @@ import br.com.certacon.certabotloadfiles.model.LoadFilesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface LoadFilesRepository extends JpaRepository<LoadFilesModel, Long> {
+public interface LoadFilesRepository extends JpaRepository<LoadFilesModel, UUID> {
 }
