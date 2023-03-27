b920a6b359a5bb280b47a72b4c974ebd27e5bdbc
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-27 11:24:14 -0300

    chore: correção sobre tipagem do id e adição de propriedades

--- src/main/java/br/com/certacon/certabotloadfiles/dto/LoadFilesDto.java
+++ src/main/java/br/com/certacon/certabotloadfiles/dto/LoadFilesDto.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Getter
 @Setter
@@ -13,13 +14,23 @@ import java.util.Date;
 @NoArgsConstructor
 public class LoadFilesDto {
     @JsonProperty(value = "file_id")
-    private Long id;
+    private UUID id;
+
     @JsonProperty(value = "pasta", required = true)
-    private String folder;
-    @JsonProperty(value = "servidor", required = true)
-    private String server;
+    private String serverFolder;
+
+    @JsonProperty(value = "pasta_cnpj", required = true)
+    private String cnpjFolder;
+
+    @JsonProperty(value = "pasta_ano", required = true)
+    private String yearFolder;
+
+    @JsonProperty(value = "caminho")
+    private String path;
+
     @JsonProperty(value = "criado_em", required = true)
     private Date createdAt;
+
     @JsonProperty(value = "atualizado_em")
     private Date updatedAt;
 }
