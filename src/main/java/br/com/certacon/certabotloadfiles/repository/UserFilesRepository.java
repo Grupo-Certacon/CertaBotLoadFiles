283293530fc77398e18c15d2330c3c191569fe59
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-28 09:29:00 -0300

    chore: criação de repository

---
+++ src/main/java/br/com/certacon/certabotloadfiles/repository/UserFilesRepository.java
@@ -0,0 +1,13 @@
+package br.com.certacon.certabotloadfiles.repository;
+
+import br.com.certacon.certabotloadfiles.model.UserFilesModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserFilesRepository extends JpaRepository<UserFilesModel, UUID> {
+    Optional<UserFilesModel> findByFileNameAndPath(String fileName, String path);
+}
