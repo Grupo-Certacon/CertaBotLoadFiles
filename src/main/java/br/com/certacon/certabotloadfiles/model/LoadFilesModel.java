1d381ee4176e5694c88b4c189ce728596484db39
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-28 17:36:51 -0300

    docs: documentação de model

--- src/main/java/br/com/certacon/certabotloadfiles/model/LoadFilesModel.java
+++ src/main/java/br/com/certacon/certabotloadfiles/model/LoadFilesModel.java
@@ -1,6 +1,7 @@
 package br.com.certacon.certabotloadfiles.model;
 
 import br.com.certacon.certabotloadfiles.utils.StatusFile;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
@@ -19,28 +20,36 @@ public class LoadFilesModel {
     @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
     @GeneratedValue(generator = "UUIDGenerator")
     @Column(name = "load_file_id")
+    @JsonProperty(value = "load_file_id")
     private UUID id;
 
     @Column(name = "pasta_servidor", nullable = false)
+    @JsonProperty(required = true, value = "pasta_servidor")
     private String serverFolder;
 
     @Column(name = "pasta_cnpj", nullable = false)
+    @JsonProperty(required = true, value = "pasta_cnpj")
     private String cnpjFolder;
 
     @Column(name = "pasta_ano", nullable = false)
+    @JsonProperty(required = true, value = "pasta_ano")
     private String yearFolder;
 
     @Column(name = "caminho")
+    @JsonProperty(value = "caminho")
     private String path;
 
     @Column(name = "status")
+    @JsonProperty(value = "status")
     @Enumerated(EnumType.STRING)
     private StatusFile status;
 
     @Column(name = "criado_em", nullable = false)
+    @JsonProperty(value = "criado_em")
     private Date createdAt;
 
     @Column(name = "atualizado_em")
+    @JsonProperty(value = "atualizado_em")
     private Date updatedAt;
 
 }
