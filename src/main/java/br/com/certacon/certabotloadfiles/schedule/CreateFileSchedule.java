e0cd3f23a2e04c74348f0079bda3e7dc5b601118
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-28 17:35:02 -0300

    feat: criação de schedule

---
+++ src/main/java/br/com/certacon/certabotloadfiles/schedule/CreateFileSchedule.java
@@ -0,0 +1,35 @@
+package br.com.certacon.certabotloadfiles.schedule;
+
+import br.com.certacon.certabotloadfiles.component.CreateFileComponent;
+import br.com.certacon.certabotloadfiles.model.LoadFilesModel;
+import br.com.certacon.certabotloadfiles.repository.LoadFilesRepository;
+import br.com.certacon.certabotloadfiles.utils.StatusFile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CreateFileSchedule {
+    private final CreateFileComponent createFileComponent;
+    private final LoadFilesRepository loadFilesRepository;
+
+    public CreateFileSchedule(CreateFileComponent createFileComponent, LoadFilesRepository loadFilesRepository) {
+        this.createFileComponent = createFileComponent;
+        this.loadFilesRepository = loadFilesRepository;
+    }
+
+    @Scheduled(fixedRate = 30000)
+    public boolean createFileScheduled() {
+        List<LoadFilesModel> modelList = loadFilesRepository.findAll();
+        Boolean check = Boolean.FALSE;
+        if (!modelList.isEmpty()) {
+            for (int i = 0; i < modelList.size(); i++) {
+                if (modelList.get(i).getStatus() == StatusFile.CREATED || modelList.get(i).getStatus() == StatusFile.UPDATED) {
+                    check = createFileComponent.checkFile(modelList.get(i).getPath());
+                }
+            }
+        }
+        return check;
+    }
+}
