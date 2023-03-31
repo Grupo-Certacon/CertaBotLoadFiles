3975dcc12f097fd81ac1ea3b41c013fe2fbe42f7
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-03-31 18:00:45 -0300

    feat: criação de objetos virtuais

---
+++ src/main/java/br/com/certacon/certabotloadfiles/vo/ProcessFileVO.java
@@ -0,0 +1,19 @@
+package br.com.certacon.certabotloadfiles.vo;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ProcessFileVO {
+    private String url_de_download;
+    private String senha;
+    private String url_de_upload;
+    private String usuario;
+    private String caminho_de_destino_download;
+    private String nome_arquivo;
+    private String id_arquivo;
+
+}
