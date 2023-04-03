463f5cc7b0dd44ad6dc25ea0a13f849488b214db
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-03 11:51:04 -0300

    test(feat): criação de teste para PostRestTemplateService

---
+++ src/test/java/br/com/certacon/certabotloadfiles/service/PostRestTemplateServiceTest.java
@@ -0,0 +1,15 @@
+package br.com.certacon.certabotloadfiles.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class PostRestTemplateServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
