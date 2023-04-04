40c3e390e2e1db24a89a995ab85676206949c954
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-04 08:11:05 -0300

    chore: melhoramento de lógica do código

--- src/main/java/br/com/certacon/certabotloadfiles/component/CreateFileComponent.java
+++ src/main/java/br/com/certacon/certabotloadfiles/component/CreateFileComponent.java
@@ -5,37 +5,27 @@ import br.com.certacon.certabotloadfiles.model.UserFilesModel;
 import br.com.certacon.certabotloadfiles.repository.UserFilesRepository;
 import br.com.certacon.certabotloadfiles.utils.StatusFile;
 import net.lingala.zip4j.ZipFile;
-import net.lingala.zip4j.exception.ZipException;
-import net.lingala.zip4j.model.FileHeader;
-import net.lingala.zip4j.model.ZipParameters;
-import net.lingala.zip4j.model.enums.CompressionLevel;
-import net.lingala.zip4j.model.enums.CompressionMethod;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.util.FileSystemUtils;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 
 @Component
 public class CreateFileComponent {
     private final UserFilesRepository userFilesRepository;
 
-
     public CreateFileComponent(UserFilesRepository userFilesRepository) {
         this.userFilesRepository = userFilesRepository;
     }
 
-    public void extractAndCompress(String inputFilePath, String outputFilePath) throws IOException {
+  /* public void extractAndCompress(String inputFilePath, String outputFilePath) throws IOException {
         // Cria um arquivo temporário para armazenar os arquivos extraídos
         File tempDir = new File("temp");
         if (!tempDir.exists()) {
@@ -105,32 +95,32 @@ public class CreateFileComponent {
             }
         }
         dir.delete();
-    }
+    }*/
 
 
     public Boolean checkFile(String path, String cnpj, String ipServer) throws IOException {
         Boolean isCreated = Boolean.FALSE;
         Path fullPath = Paths.get(path);
-        File[] file = new File(fullPath.toUri()).listFiles();
+        File[] file = new File(fullPath.toString()).listFiles();
         for (int i = 0; i < file.length; i++) {
             Optional<UserFilesModel> filePath = userFilesRepository.findByFileName(file[i].getName());
             if (!filePath.isPresent()) {
                 if (FilenameUtils.getExtension(file[i].getName()).equals("txt")) {
-                    String zipPath = file[i].getPath().replace(".txt", ".zip");
+                    String zipPath = file[i].getPath().replace(FilenameUtils.getExtension(file[i].getName()), "zip");
+                    Path pathForSave = Path.of(zipPath);
                     ZipFile zipFile = new ZipFile(zipPath);
                     try {
                         zipFile.addFile(file[i]);
                         zipFile.close();
                         FileSystemUtils.deleteRecursively(file[i]);
-                        Path pathForSave = Path.of(zipPath);
                         String mimeType = Files.probeContentType(pathForSave);
                         String ext = FilenameUtils.getExtension(pathForSave.toString());
                         UserFilesModel userFilesModelSaved = UserFilesModel.builder()
                                 .cnpj(cnpj)
                                 .ipServer(ipServer)
                                 .mimeType(mimeType)
-                                .fileName(file[i].getName())
-                                .path(file[i].getPath())
+                                .fileName(pathForSave.getFileName().toString())
+                                .path(zipPath)
                                 .extension(ext)
                                 .status(StatusFile.CREATED)
                                 .createdAt(new Date())
