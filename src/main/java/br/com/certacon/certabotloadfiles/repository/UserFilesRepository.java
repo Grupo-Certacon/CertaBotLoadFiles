5899e96be8e8968f33813242435756fdbd2cb651
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-04-19 11:05:26 -0300

    chore: remoção de metodo inutilizado

--- src/main/java/br/com/certacon/certabotloadfiles/repository/UserFilesRepository.java
+++ src/main/java/br/com/certacon/certabotloadfiles/repository/UserFilesRepository.java
@@ -4,13 +4,10 @@ import br.com.certacon.certabotloadfiles.model.UserFilesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserFilesRepository extends JpaRepository<UserFilesModel, UUID> {
     Optional<UserFilesModel> findByFileName(String fileName);
-
-    List<UserFilesModel> findAllByExtension(String extension);
 }
