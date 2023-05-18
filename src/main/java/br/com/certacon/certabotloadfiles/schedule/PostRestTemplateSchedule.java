76e66f088394c940f9af41103002d5e510b50abe
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-18 09:53:03 -0300

    fix: adaptação para outro bot

--- src/main/java/br/com/certacon/certabotloadfiles/schedule/PostRestTemplateSchedule.java
+++ src/main/java/br/com/certacon/certabotloadfiles/schedule/PostRestTemplateSchedule.java
@@ -4,32 +4,24 @@ import br.com.certacon.certabotloadfiles.model.UserFilesModel;
 import br.com.certacon.certabotloadfiles.repository.UserFilesRepository;
 import br.com.certacon.certabotloadfiles.service.PostRestTemplateService;
 import br.com.certacon.certabotloadfiles.utils.StatusFile;
-import br.com.certacon.certabotloadfiles.vo.ArquivoEfdModelVO;
-import br.com.certacon.certabotloadfiles.vo.ArquivoEfdVO;
-import br.com.certacon.certabotloadfiles.vo.ProcessFileModelVO;
-import br.com.certacon.certabotloadfiles.vo.ProcessFileVO;
-import org.springframework.beans.factory.annotation.Value;
+import br.com.certacon.certabotloadfiles.vo.FileEntityVO;
+import br.com.certacon.certabotloadfiles.vo.FileVO;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 public class PostRestTemplateSchedule {
     private final PostRestTemplateService postRestTemplateService;
     private final UserFilesRepository userFilesRepository;
-    @Value("${config.downloadPath}")
-    private String downloadPath;
-
-    @Value("${config.dockerPathDownload}")
-    private String dockerPathDownload;
 
     public PostRestTemplateSchedule(PostRestTemplateService postRestTemplateService, UserFilesRepository userFilesRepository) {
         this.postRestTemplateService = postRestTemplateService;
         this.userFilesRepository = userFilesRepository;
     }
 
-    @Scheduled(fixedRate = 30000, initialDelay = 90000)
+    @Scheduled(fixedRate = 30000, initialDelay = 45000)
     public boolean postRest() {
         List<UserFilesModel> modelList = userFilesRepository.findAll();
         boolean check = Boolean.FALSE;
@@ -37,35 +29,23 @@ public class PostRestTemplateSchedule {
             for (int i = 0; i < modelList.size(); i++) {
                 modelList.get(i).getId().toString();
                 if (modelList.get(i).getStatus() == StatusFile.CREATED || modelList.get(i).getStatus() == StatusFile.UPDATED) {
-                    ArquivoEfdVO arquivoEfdVO = ArquivoEfdVO.builder()
-                            .clientCnpj(modelList.get(i).getCnpj())
-                            .name("Certacon")
+                    FileVO fileVO = FileVO.builder()
+                            .cnpj(modelList.get(i).getCnpj())
+                            .ipServer(modelList.get(i).getIpServer())
+                            .fileName(modelList.get(i).getFileName())
+                            .filePath(modelList.get(i).getPath())
                             .build();
-                    ArquivoEfdModelVO result = postRestTemplateService.enviarArquivoEfd(arquivoEfdVO).getBody();
+                    FileEntityVO result = postRestTemplateService.createFile(fileVO).getBody();
                     if (result != null) {
-                        ProcessFileVO processFileVO = ProcessFileVO.builder()
-                                .id_arquivo(result.getId().toString())
-                                .usuario("giovanni.andrade@certacon.com.br")
-                                .senha("1")
-                                .caminho_de_arquivo(modelList.get(i).getPath())
-                                .caminho_de_destino_download(downloadPath)
-                                .url_de_upload("http://" + modelList.get(i).getIpServer() + "/tributario")
-                                .url_de_download(dockerPathDownload + modelList.get(i).getId().toString())
-                                .nome_arquivo(modelList.get(i).getFileName())
-                                .build();
-                        ProcessFileModelVO processResult = postRestTemplateService.createProcess(processFileVO).getBody();
-                        if (processResult != null) {
-                            modelList.get(i).setProcessId(processResult.getId());
-                            modelList.get(i).setStatus(StatusFile.UPLOADED);
-                            check = Boolean.TRUE;
-                        } else {
-                            modelList.get(i).setStatus(StatusFile.ERROR);
-                        }
+                        modelList.get(i).setStatus(StatusFile.UPLOADED);
+                        check = Boolean.TRUE;
                     } else {
                         modelList.get(i).setStatus(StatusFile.ERROR);
                     }
-                    userFilesRepository.save(modelList.get(i));
+                } else {
+                    modelList.get(i).setStatus(StatusFile.ERROR);
                 }
+                userFilesRepository.save(modelList.get(i));
             }
         }
         return check;
--- src/main/java/br/com/certacon/certabotloadfiles/service/PostRestTemplateService.java
+++ src/main/java/br/com/certacon/certabotloadfiles/service/PostRestTemplateService.java
@@ -1,10 +1,8 @@
 package br.com.certacon.certabotloadfiles.service;
 
 import br.com.certacon.certabotloadfiles.exception.BadRequestException;
-import br.com.certacon.certabotloadfiles.vo.ArquivoEfdModelVO;
-import br.com.certacon.certabotloadfiles.vo.ArquivoEfdVO;
-import br.com.certacon.certabotloadfiles.vo.ProcessFileModelVO;
-import br.com.certacon.certabotloadfiles.vo.ProcessFileVO;
+import br.com.certacon.certabotloadfiles.vo.FileEntityVO;
+import br.com.certacon.certabotloadfiles.vo.FileVO;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -15,32 +13,17 @@ public class PostRestTemplateService {
     private RestTemplate restTemplate;
 
     public PostRestTemplateService() {
-        this.restTemplate = new RestTemplateBuilder().rootUri("http://192.168.1.64:8092/certabot").build();
+        this.restTemplate = new RestTemplateBuilder().rootUri("http://192.168.1.46:8068/CertaBotOrganize").build();
     }
 
-    public ResponseEntity<ArquivoEfdModelVO> enviarArquivoEfd(ArquivoEfdVO arquivoEfdVO) {
-        ResponseEntity<ArquivoEfdModelVO> resposta = null;
-        try {
-            // Crie um objeto HttpEntity contendo o objeto ArquivoEfdVO como corpo da solicitação POST
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<ArquivoEfdVO> requestEntity = new HttpEntity<>(arquivoEfdVO, headers);
-            // Faça a solicitação POST e obtenha a resposta como uma instância de ArquivoEfdVO
-            resposta = restTemplate.exchange("/arquivo/efd", HttpMethod.POST, requestEntity, ArquivoEfdModelVO.class);
-        } catch (RuntimeException e) {
-            throw new RuntimeException("Objeto não encontrado");
-        }
-        return resposta;
-    }
-
-    public ResponseEntity<ProcessFileModelVO> createProcess(ProcessFileVO processFileVO) {
-        ResponseEntity<ProcessFileModelVO> resposta = null;
+    public ResponseEntity<FileEntityVO> createFile(FileVO fileVO) {
+        ResponseEntity<FileEntityVO> resposta = null;
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<ProcessFileVO> requestEntity = new HttpEntity<>(processFileVO, headers);
+            HttpEntity<FileVO> requestEntity = new HttpEntity<>(fileVO, headers);
             // Faça a solicitação POST e obtenha a resposta como uma instância de ArquivoEfdVO
-            resposta = restTemplate.exchange("/process/file", HttpMethod.POST, requestEntity, ProcessFileModelVO.class);
+            resposta = restTemplate.exchange("/files", HttpMethod.POST, requestEntity, FileEntityVO.class);
         } catch (BadRequestException e) {
             throw new BadRequestException();
         }
