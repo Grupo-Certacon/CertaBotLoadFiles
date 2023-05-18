78b667f2077a52d0c85c48449dc0c67177a0b66a
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-05-18 09:52:40 -0300

    chore: implementação de metodos da componente de criação de paths

--- src/main/java/br/com/certacon/certabotloadfiles/schedule/PathCreationSchedule.java
+++ src/main/java/br/com/certacon/certabotloadfiles/schedule/PathCreationSchedule.java
@@ -2,34 +2,64 @@ package br.com.certacon.certabotloadfiles.schedule;
 
 import br.com.certacon.certabotloadfiles.component.PathCreationComponent;
 import br.com.certacon.certabotloadfiles.model.LoadFilesModel;
+import br.com.certacon.certabotloadfiles.model.UserFilesModel;
 import br.com.certacon.certabotloadfiles.repository.LoadFilesRepository;
+import br.com.certacon.certabotloadfiles.repository.UserFilesRepository;
+import br.com.certacon.certabotloadfiles.utils.FileFunctions;
 import br.com.certacon.certabotloadfiles.utils.StatusFile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
 
 @Component
 public class PathCreationSchedule {
     private final PathCreationComponent pathCreationComponent;
     private final LoadFilesRepository loadFilesRepository;
+    private final UserFilesRepository userFilesRepository;
 
-    public PathCreationSchedule(PathCreationComponent pathCreationComponent, LoadFilesRepository loadFilesRepository) {
+    public PathCreationSchedule(PathCreationComponent pathCreationComponent, LoadFilesRepository loadFilesRepository,
+                                UserFilesRepository userFilesRepository) {
         this.pathCreationComponent = pathCreationComponent;
         this.loadFilesRepository = loadFilesRepository;
+        this.userFilesRepository = userFilesRepository;
     }
 
     @Scheduled(fixedRate = 30000)
     public boolean pathCreate() {
         List<LoadFilesModel> modelList = loadFilesRepository.findAll();
-        Boolean created = Boolean.FALSE;
         if (!modelList.isEmpty()) {
-            for (int i = 0; i < modelList.size(); i++) {
-                if (modelList.get(i).getStatus() == StatusFile.CREATED || modelList.get(i).getStatus() == StatusFile.UPDATED) {
-                    created = pathCreationComponent.checkPath(modelList.get(i).getId());
+            modelList.forEach(loadFile -> {
+                if (loadFile.getStatus() == StatusFile.CREATED || loadFile.getStatus() == StatusFile.UPDATED) {
+                    loadFile = pathCreationComponent.createOrganizedPath(loadFile.getId(), FileFunctions.ORGANIZADOS);
+                    if (loadFile.getStatus() == StatusFile.CREATEDORGANIZED) {
+                        loadFile = pathCreationComponent.createSentPath(loadFile.getId(), FileFunctions.ENVIADOS);
+                    }
+                    if (loadFile.getStatus() == StatusFile.CREATEDSENT) {
+                        loadFile = pathCreationComponent.createArchivedPath(loadFile.getId(), FileFunctions.ARQUIVADOS);
+                    }
+                    if (loadFile.getStatus() == StatusFile.CREATEDARCHIVED) {
+                        loadFile = pathCreationComponent.createOrganizePath(loadFile.getId(), FileFunctions.ORGANIZAR);
+                    }
+                    if (loadFile.getStatus() == StatusFile.CREATEDORGANIZE) {
+                        loadFile = pathCreationComponent.createLoadPath(loadFile.getId());
+                        loadFilesRepository.save(loadFile);
+                        UserFilesModel filesModel = UserFilesModel.builder()
+                                .path(loadFile.getPath())
+                                .ipServer(loadFile.getServerFolder())
+                                .createdAt(new Date())
+                                .year(loadFile.getYearFolder())
+                                .fileName(loadFile.getYearFolder())
+                                .cnpj(loadFile.getCnpjFolder())
+                                .status(StatusFile.CREATED)
+                                .build();
+                        userFilesRepository.save(filesModel);
+                    }
                 }
-            }
+            });
+            return Boolean.TRUE;
         }
-        return created;
+        return Boolean.FALSE;
     }
 }
