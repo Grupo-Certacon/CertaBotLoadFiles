c7661d4bad44345b8d2d3d7b319769010c91db0d
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-07-06 07:32:11 -0300

    feat: implemented kafka Message serializer for producer

---
+++ src/main/java/br/com/certacon/certabotloadfiles/kafka/producer/ProducerMessageSerializer.java
@@ -0,0 +1,7 @@
+package br.com.certacon.certabotloadfiles.kafka.producer;
+
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+public class ProducerMessageSerializer extends JsonSerializer<ProducerMesssage> {
+
+}
