2ece6a565e6c312c4893ebc3708ba27629216fb5
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-07-06 07:33:28 -0300

    feat: upload message entity created

---
+++ src/main/java/br/com/certacon/certabotloadfiles/kafka/consumer/UploadMesssage.java
@@ -0,0 +1,16 @@
+package br.com.certacon.certabotloadfiles.kafka.consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UploadMesssage {
+
+    private String Body;
+
+}
