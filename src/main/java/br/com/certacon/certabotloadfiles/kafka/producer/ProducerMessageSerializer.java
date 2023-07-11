277b6264f64caca0248e9de002a0857e678df6ba
giovanniandradecertacon <giovanni.andrade@certacon.com.br>
2023-07-11 16:15:41 -0300

    chore: deleted unused kafka services

--- src/main/java/br/com/certacon/certabotloadfiles/kafka/producer/ProducerMessageSerializer.java
+++
@@ -1,7 +0,0 @@
-package br.com.certacon.certabotloadfiles.kafka.producer;
-
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-public class ProducerMessageSerializer extends JsonSerializer<ProducerMesssage> {
-
-}
